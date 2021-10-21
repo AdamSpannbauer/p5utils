f4d318d665ec97961fd541fb2f1a21fc92f48b6c
AdamSpannbauer <spannbaueradam@gmail.com>
2021-10-21 13:12:55 -0400

    add maskedNoisyLine()

--- noiseUtils.js
+++ noiseUtils.js
@@ -8,6 +8,18 @@ export const addNoise = ({ x, y }, nScl = 0.001, nSize = 32) => {
   return { x: x + dx, y: y + dy };
 };
 
+const setNPoints = (x1, y1, x2, y2) => {
+  const d = dist(x1, y1, x2, y2);
+  const maxPoints = 512;
+  // eslint-disable-next-line no-param-reassign
+  let nPoints = map(d, 0, width, 0, maxPoints);
+
+  // eslint-disable-next-line no-param-reassign
+  if (nPoints < 2) nPoints = 2;
+
+  return nPoints;
+};
+
 export const noisyLine = (
   x1, y1, x2, y2, nScl = 0.001, nSize = 30, nPoints = null,
 ) => {
@@ -15,10 +27,8 @@ export const noisyLine = (
   noFill();
 
   if (nPoints === null) {
-    const d = dist(x1, y1, x2, y2);
-    const maxPoints = 512;
-    // eslint-disable-next-line no-param-reassign
-    nPoints = map(d, 0, width, 0, maxPoints);
+  // eslint-disable-next-line no-param-reassign
+    nPoints = setNPoints(x1, y1, x2, y2);
   }
 
   // eslint-disable-next-line no-param-reassign
@@ -38,9 +48,51 @@ export const noisyLine = (
 };
 
 export const maskedNoisyLine = (
-  x1, y1, x2, y2, nScl = 0.001, nSize = 30, testFunc = () => true, nPoints = null,
+  x1, y1, x2, y2, testFunc = () => true, nScl = 0.001, nSize = 30, nPoints = null,
 ) => {
+  push();
+  noFill();
 
+  if (nPoints === null) {
+    // eslint-disable-next-line no-param-reassign
+    nPoints = setNPoints(x1, y1, x2, y2);
+  }
+
+  // eslint-disable-next-line no-param-reassign
+  if (nPoints < 2) nPoints = 2;
+
+  let drawing = false;
+  let shapeStarted = false;
+  for (let i = 0; i < nPoints; i += 1) {
+    const xi = map(i, 0, nPoints - 1, x1, x2);
+    const yi = map(i, 0, nPoints - 1, y1, y2);
+
+    const pt = addNoise({ x: xi, y: yi }, nScl, nSize);
+    const drawPt = testFunc(pt);
+    if (drawPt) {
+      // Don't start drawing with first point; need 2 pts for a line...
+      if (!drawing) {
+        drawing = true;
+        // eslint-disable-next-line no-continue
+        continue;
+      }
+
+      if (!shapeStarted) {
+        beginShape();
+        shapeStarted = true;
+      }
+
+      const { x: noisyX, y: noisyY } = pt;
+      vertex(noisyX, noisyY);
+    } else {
+      if (drawing && shapeStarted) endShape();
+      drawing = false;
+      shapeStarted = false;
+    }
+  }
+  if (drawing && shapeStarted) endShape();
+
+  pop();
 };
 
 export const noisyEllipse = (x, y, xr, yr, nScl = 0.001, nSize = 30) => {
--- testSketch.js
+++ testSketch.js
@@ -1,4 +1,4 @@
-import { noisyLine } from './noiseUtils.js';
+import { maskedNoisyLine, noisyLine } from './noiseUtils.js';
 
 window.setup = () => {
   createCanvas(512, 512);
@@ -8,4 +8,9 @@ window.draw = () => {
   background(200);
 
   noisyLine(0, 0, width, height);
+
+  const testFunc = ({ x, y }) => dist(x, y, width / 2, height / 2) < 200;
+  for (let y = 0; y < height; y += 10) {
+    maskedNoisyLine(0, y, width, y, testFunc);
+  }
 };

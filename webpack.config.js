b037ae47e2ed0f898fd46f4ecd0539d1c4eaf849
Adam Spannbauer <SpannbauerAdam@gmail.com>
2021-10-23 09:35:09 -0400

    include min in output file name

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'p5utils.js',
+    filename: 'p5utils.min.js',
     library: {
       name: 'p5utils',
       type: 'umd',
